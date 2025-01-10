8e2c7b7198fa257bbb274fba6889c4be306ef286
Jiayue <jiayue.zh@outlook.com>
2025-01-10 14:04:12 +0800

    Initialize Spring Boot project

---
+++ src/main/java/com/chameleon/cilicili/CiliciliApplication.java
@@ -0,0 +1,13 @@
+package com.chameleon.cilicili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiliciliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiliciliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chameleon/cilicili/CiliciliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chameleon.cilicili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiliciliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
